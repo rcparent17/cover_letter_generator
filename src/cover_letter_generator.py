3e85969c9b1f3219b6e13f6864d5681f7b6336ab
rcparent17 <reilly.c.parent@gmail.com>
2025-08-28 14:15:45 -0400

    finalize project structure

--- src/cover_letter_generator.py
+++ src/cover_letter_generator.py
@@ -1,6 +1,8 @@
 import sys, os
 
 import yaml
+import pdfkit, pypdf
+import argparse
 
 '''
 COMPANY YAML ENTRY TEMPLATE:
@@ -23,5 +25,17 @@ COMPANY YAML ENTRY TEMPLATE:
 
 # TODO
 class CoverLetterGenerator:
-    def __init__(self):
-        pass
+    def __init__(self, template_file="deps/template.txt", resume_file="deps/reilly_parent_devops_resume.pdf", companies_file="deps/companies.yaml"):
+        if not (os.path.exists(template_file) or os.path.exists(resume_file) or os.path.exists(companies_file)):
+            raise FileExistsError("One or more provided files does not exist.")
+        
+        # read template
+        self.template = ""
+        with open(template_file, "r") as template:
+            self.template = "".join(template.readlines())
+        
+def main():
+    generator = CoverLetterGenerator()
+
+if __name__ == "__main__":
+    main()
