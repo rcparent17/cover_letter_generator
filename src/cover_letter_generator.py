ca915feeb401b85e74a953811b0d2185f830fb87
rcparent17 <reilly.c.parent@gmail.com>
2025-08-28 20:33:06 -0400

    add argparse config

--- src/cover_letter_generator.py
+++ src/cover_letter_generator.py
@@ -25,7 +25,7 @@ COMPANY YAML ENTRY TEMPLATE:
 
 # TODO
 class CoverLetterGenerator:
-    def __init__(self, template_file="deps/template.txt", resume_file="deps/reilly_parent_devops_resume.pdf", companies_file="deps/companies.yaml"):
+    def __init__(self, template_file, resume_file, companies_file, output_dir):
         if not (os.path.exists(template_file) or os.path.exists(resume_file) or os.path.exists(companies_file)):
             raise FileExistsError("One or more provided files does not exist.")
         
@@ -33,9 +33,18 @@ class CoverLetterGenerator:
         self.template = ""
         with open(template_file, "r") as template:
             self.template = "".join(template.readlines())
-        
+
+def collect_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", "--resume-file", required=True)
+    parser.add_argument("-c", "--companies-file", required=True)
+    parser.add_argument("-t", "--template-file", required=True)
+    parser.add_argument("-o", "--output-dir", required=True)
+    return parser.parse_args()
+
 def main():
-    generator = CoverLetterGenerator()
+    args = collect_args()
+    generator = CoverLetterGenerator(args.template_file, args.resume_file, args.companies_file, args.output_dir)
 
 if __name__ == "__main__":
     main()
