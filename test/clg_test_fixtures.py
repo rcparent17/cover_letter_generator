170b7b092d24add96dfa7601c2e39a7e5b994404
rcparent17 <reilly.c.parent@gmail.com>
2025-09-02 14:03:35 -0400

    split test fixtures into own subdir

---
+++ test/clg_test_fixtures.py
@@ -0,0 +1,107 @@
+import os
+
+import pytest
+
+TMP_FILES = []
+
+@pytest.fixture
+def companies_yaml(tmpdir):
+    companies_yaml = '''companies:
+  - name: "comp1"
+    location: "nowhere"
+    job_title: "freeloader"
+    requirements:
+      - "eat food"
+      - "sleep"
+      - "die"
+      - "ascend"
+    qualifications:
+      - "i eat"
+      - "i sleep"
+      - "i will die"
+      - "i will ascend"
+  - name: "comp2"
+    location: "anywhere"
+    job_title: "anything"
+    requirements:
+      - "wake up"
+      - "eat"
+      - "eat again"
+      - "sleep"
+    qualifications:
+      - "i wake up"
+      - "i eat"
+      - "i still eat"
+      - "i sleep"
+    '''.strip()
+    tmp_file_path = tmpdir.join("tmp_companies.yaml")
+    TMP_FILES.append(tmp_file_path)
+    with open(tmp_file_path, "w") as tmp_yaml_file:
+        tmp_yaml_file.write(companies_yaml)
+    yield tmp_file_path
+
+@pytest.fixture
+def invalid_yaml(tmpdir):
+    companies_yaml = ''':
+  - name: "comp1"
+    location: "nowhere"
+    job_title: "freeloader"
+    requirements:
+      - "eat food"
+      - "sleep"
+      - "die"
+      - "ascend"
+    qualifications:
+      - "i eat"
+      - "i sleep"
+      - "i will die"
+      - "i will ascend"
+  - name: "comp2"
+    location: "anywhere"
+    job_title: "anything"
+    requirements:
+      - "wake up"
+      - "eat"
+      - "eat again"
+      - "sleep"
+    qualifications:
+      - "i wake up"
+      - "i eat"
+      - "i still eat"
+      - "i sleep"
+    '''.strip()
+    tmp_file_path = tmpdir.join("invalid_companies.yaml")
+    TMP_FILES.append(tmp_file_path)
+    with open(tmp_file_path, "w") as tmp_yaml_file:
+        tmp_yaml_file.write(companies_yaml)
+    yield tmp_file_path
+
+@pytest.fixture
+def missing_yaml(tmpdir):
+    companies_yaml = '''companies:
+  - name: "comp1"
+    requirements:
+      - "eat food"
+      - "sleep"
+      - "die"
+      - "ascend"
+    qualifications:
+      - "i eat"
+      - "i sleep"
+      - "i will die"
+      - "i will ascend"
+  - name: "comp2"
+    location: "anywhere"
+    job_title: "anything"
+    '''.strip()
+    tmp_file_path = tmpdir.join("invalid_companies.yaml")
+    TMP_FILES.append(tmp_file_path)
+    with open(tmp_file_path, "w") as tmp_yaml_file:
+        tmp_yaml_file.write(companies_yaml)
+    yield tmp_file_path
+
+def clean_tmpdir():
+    for i in range(len(TMP_FILES)):
+        os.remove(str(TMP_FILES[i]))
+    for i in range(len(TMP_FILES)):
+        del TMP_FILES[i]
