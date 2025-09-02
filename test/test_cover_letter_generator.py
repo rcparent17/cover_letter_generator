645db1e9ab01bbbbe613cbe8dbf9faf6bea012e2
rcparent17 <reilly.c.parent@gmail.com>
2025-09-02 14:22:53 -0400

    add generator test stubs

--- test/test_cover_letter_generator.py
+++ test/test_cover_letter_generator.py
@@ -1,4 +1,23 @@
 import sys, os
 sys.path.append(os.path.join(os.getcwd(), "src"))
 from cover_letter_generator import CoverLetterGenerator
+from clg_test_fixtures import *
 import pytest
+
+def test_generate_letter_filename(company):
+    pass
+
+def test_generate_resume_filename(company):
+    pass
+
+def test_populate_template(template, company):
+    pass
+
+def test_clg_constructor(template_file, resume_file, companies_file, output_dir):
+    pass
+
+def test_generate_letter(generator):
+    pass
+
+def test_output_merged_pdf(generator):
+    pass
