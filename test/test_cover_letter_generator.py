33751c62d3ad519feecb116bcb8dc51db394ef6a
rcparent17 <reilly.c.parent@gmail.com>
2025-09-04 17:33:45 -0400

    add generator filename generation unit tests

--- test/test_cover_letter_generator.py
+++ test/test_cover_letter_generator.py
@@ -1,14 +1,20 @@
 import sys, os
 sys.path.append(os.path.join(os.getcwd(), "src"))
 from cover_letter_generator import CoverLetterGenerator
+import clg_helpers
 from clg_test_fixtures import *
 import pytest
 
+@pytest.fixture
+def company(companies_yaml):
+    companies = clg_helpers.read_companies(companies_yaml)
+    yield companies[0]
+
 def test_generate_letter_filename(company):
-    pass
+    assert CoverLetterGenerator._generate_letter_filename(company) == "reilly_parent_comp1_freeloader_cover_letter.pdf"
 
 def test_generate_resume_filename(company):
-    pass
+    assert CoverLetterGenerator._generate_resume_filename(company) == "reilly_parent_comp1_freeloader_resume.pdf"
 
 def test_populate_template(template, company):
     pass
